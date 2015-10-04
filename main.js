e6e9dc13db0189b16396ecb2e4bc705037b60105
beccaades <rebecca.ades@gmail.com>
2015-10-03 22:17:45 -0400

    nav bar and top almost done

---
+++ main.js
@@ -0,0 +1,13 @@
+$(function(){
+  //load home page when page loads
+  $("#reponse").load("home.html");
+  $("#home").click(function(){
+    $("#response").load("home.html");
+  });
+  $("#about").click(function(){
+    $("#response").load("about.html"); 
+  });
+  $("#development").click(function(){
+    $("#response").load("development.html");
+  });
+});
