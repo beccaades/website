657892b3ffd0e9d15fe7eca6e0ccee705d2d5a65
beccaades <rebecca.ades@gmail.com>
2015-10-04 01:07:53 -0400

    added image and heading

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 $(function(){
   //load home page when page loads
-  $("#reponse").load("home.html");
+  $("#reponse").load("/home.html");
   $("#home").click(function(){
-    $("#response").load("home.html");
+    $("#response").load("/home.html");
   });
   $("#about").click(function(){
-    $("#response").load("about.html"); 
+    $("#response").load("/about.html"); 
   });
   $("#development").click(function(){
-    $("#response").load("development.html");
+    $("#response").load("/development.html");
   });
 });
